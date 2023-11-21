f88f7bd57955b4e8e6bda60c370e7f4d8a412652
sergi0ps <sergio.pinsan@educa.jcyl.es>
2023-11-21 14:33:01 +0100

    combinacio

--- PrimitivaBuena/src/es/juana/daw/Combinacion.java
+++ PrimitivaBuena/src/es/juana/daw/Combinacion.java
@@ -1,5 +1,14 @@
 package es.juana.daw;
 
-public class Combinacion {
+import java.util.Scanner;
 
+public class Combinacion {
+	private int[] numero = new int[6];
+	private int reintegro;
+	public int compararNumeros() {
+		Scanner sc = new Scanner(System.in);
+		int numero = sc.nextInt();
+		
+		
+	}
 }
