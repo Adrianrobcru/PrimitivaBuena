9f5fdfb382ca664d4a8909cd9a2b4cf231f2b6b7
Adrianrobcru <adrian.rob.cru.1@educa.jcyl.es>
2023-11-23 13:52:17 +0100

    Se ha terminado la funcion compararNumeros y se ha añadido comentarios

--- PrimitivaBuena/src/es/juana/daw/Combinacion.java
+++ PrimitivaBuena/src/es/juana/daw/Combinacion.java
@@ -8,12 +8,12 @@ public class Combinacion {
 	/*inicializamos dos variables,numReintegro que es un int donde ira el numero reintegro y un array de tipo int que se guardara los numeros de a primitiva*/
 	int numReintegro;
 	int[] numPrimitiva= new int[6];
-	/*Este metodo nos permite generar numeros aleatorios y asignarlos en la variable numReintegro y en el array numPrimitiva*/
+	/*Este constructor nos permite generar numeros aleatorios y asignarlos en la variable numReintegro y en el array numPrimitiva*/
 	
 	
 	
 	
-	public  Combinacion(){
+	public  Combinacion(){/*El constructor que genera el numero premiado*/
 		Random randomReintegro=new Random();
 		this.numReintegro= randomReintegro.nextInt(9);/*Genera un numero aleatorio de cero hasta el numero puesto en el parentesis*/
 		for(int i=0;i<this.numPrimitiva.length;i++) {/*Este for nos permite asignar a cada espacio de memoria un numero aleatorio*/
@@ -23,26 +23,82 @@ public class Combinacion {
 		
 	}
 	
-	public Combinacion(int reintegro, int[] primitiva) {
-		primitiva = new int[6];
-		this.numReintegro = reintegro;
-		for(int i = 0; i < primitiva.length; i++) {
+	public Combinacion(int reintegro, int[] primitiva) {/*El constructor que genera el numero que introduce el usuario*/
+		primitiva = new int[numPrimitiva.length];/*Indica las posiciones de memoria que va a tener el array primitiva*/
+		this.numReintegro = reintegro;/*Añade el numero reintegro a numReintegro*/
+		for(int i = 0; i < primitiva.length; i++) {/*Añade numeros al array posicion por posicion*/
 			this.numPrimitiva[i] = primitiva[i];
 		}
 	}
 	
-	public void compararNumeros() {
-		Combinacion combinacionPrimtiva = new Combinacion();
-		Scanner sc = new Scanner(System.in);
-		int reintegroLeer= sc.nextInt();
-		int [] primitivaLeer = new int[numPrimitiva.length];
-		for(int i = 0; i< this.numPrimitiva.length; i++) {
-			primitivaLeer[i]= sc.nextInt();
+	public int compararNumeros() {
+		Combinacion combinacionPrimtiva = new Combinacion();/*Se crea un objeto de tipo combinacion en este caso sera el numero premiado*/
+		Scanner sc = new Scanner(System.in);/*se llama al escaner*/
+		
+		int [] primitivaLeer = new int[numPrimitiva.length];/*se inicializa el array el cual va a ser el que introduzca el usuario*/
+		for(int i = 0; i< this.numPrimitiva.length; i++) {/*for que mete cada numero introducido por el usario en cada posicion de memoria del for*/
+			System.out.println("Introduce el "+i+"º numero de la primitiva");
+			int a=sc.nextInt();
+			if(a>0 && a<20) {/*Comprobador para ver si el numero introducido es valido*/
+			primitivaLeer[i]=a;}
+			else {
+				System.out.println("Ese numero no esta permitido,porfarvor introduzca un numero del 1 al 20");
+				i=i-1;
 			}
+			
+			}
+		boolean reintegrVal=false;
+		System.out.println("Introduce el numero de reintegro");
+		while (reintegrVal==false) {/*comprobacion de que el reintegro es correcto*/
+			int b=sc.nextInt();
+		
+			if (b>9 && b<0) {
+				System.out.println("Ese numero no esta permitido,porfavor introduzca un numero del 1 al 20");
+			}else {
+				int reintegroLeer= b;
+				reintegrVal=true;
+			}
+			
+		}
+		
 		Combinacion combinacionLeer = new Combinacion();
+		int acierto=0;
 		
-		
-		
+		for(int i =0;i<this.numPrimitiva.length;i++) {/*este for que dentro tiene otro for lo que hace es comparar una posicion de memoria con el resto de posiciones de memoria*/
+			for(int j =0;j<this.numPrimitiva.length;j++) {
+				if(combinacionPrimtiva.numPrimitiva[i]==combinacionLeer.numPrimitiva[j]) {
+					acierto=acierto+1;
+				}
+			}
+		}
+		if(combinacionPrimtiva.numReintegro==combinacionLeer.numReintegro) {/*dependiendo de los aciertos que haya y si se ha acertado el reintegro o no se devuelve un numero que con la funcion dineroGanado()'que hay que crear'*/
+			if(acierto==4) {
+				return 1;
+			}
+			if(acierto==5) {
+				return 2;
+			}
+			if(acierto==6) {
+				return 3;
+			}
+			if(acierto<4) {
+				return 4;
+			}
+		}else {
+			if(acierto==4) {
+				return 5;
+			}
+			if(acierto==5) {
+				return 6;
+			}
+			if(acierto==6) {
+				return 7;
+			}
+			if(acierto<4) {
+				return 8;
+			}
+		}
+		return 0;
 		}
 	
 }
