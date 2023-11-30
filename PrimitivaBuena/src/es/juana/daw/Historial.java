a134c54fd1fbb71925d58efb09f6dd07b742457b
aaronvalmar <aaron.valmar@gmail.com>
2023-11-30 10:24:51 +0100

    Comentarios en la clase historial

--- PrimitivaBuena/src/es/juana/daw/Historial.java
+++ PrimitivaBuena/src/es/juana/daw/Historial.java
@@ -1,25 +1,33 @@
 package es.juana.daw;
 
 public class Historial {
+	//variables
 	int aciertos;
 	int dineroGanado;
 	Combinacion combiancionGanadora;
 	Combinacion combinacionUsuario;
 	Primitiva dineroGanado2 = new Primitiva();
+	//constructor
 	public Historial(Combinacion combinacionPremiada,Combinacion combinacionIntroducida,int aciertos,int dineroGanador) {
 		this.aciertos=aciertos;
 		this.dineroGanado=dineroGanador;
 		this.combiancionGanadora=combinacionPremiada;
 		this.combinacionUsuario=combinacionIntroducida;
 	}
+	/**
+	 * Devuelve el historial de las partidas que se han jugado
+	 * @return una cadena de String que muestra el historial
+	 */
 	public String imprimirHistorial() {
 		String stringReintegro=String.valueOf(combiancionGanadora.numReintegro);
 		String stringPrimitiva = "";
 		String stringReintegro2=String.valueOf(combinacionUsuario.numReintegro);
 		String stringPrimitiva2 = "";
+		//bucle que representa la combinacion ganadora
 		for(int i=0;i<combiancionGanadora.numPrimitiva.length;i++) {
-			stringPrimitiva=stringPrimitiva+" "+String.valueOf(combiancionGanadora.numPrimitiva[i]);
+			stringPrimitiva=stringPrimitiva+" "+String.valueOf(combiancionGanadora.numPrimitiva[i]);//uso del metodo valueOf que devuelve el valor del String en forma de cadena
 		}
+		//bucle que representa la combinacion introducia por el usuario
 		for(int i=0;i<combiancionGanadora.numPrimitiva.length;i++) {
 			stringPrimitiva2=stringPrimitiva2+" "+String.valueOf(combinacionUsuario.numPrimitiva[i]);
 		}
